d54ea4c10aba752c058d7ec9403105c5ff721bbb
1900Linz <1290486803@qq.com>
2023-04-20 19:59:53 +0800

    string_replace函数优化思路

---
+++ learnLeetCode/learnLeetcode/13罗马数字转整数romanToInt/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <QtCore/QCoreApplication>
+#include <QDebug>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    void string_replace(std::string& strBig, const std::string& strsrc, const std::string& strdst) {
+        std::string::size_type pos = 0;
+        std::string::size_type srclen = strsrc.size();
+        std::string::size_type dstlen = strdst.size();
+
+        while ((pos = strBig.find(strsrc, pos)) != std::string::npos)
+        {
+            strBig.replace(pos, srclen, strdst);
+            pos += dstlen;
+        }
+    }
+
+#if 0
+    int romanToInt(std::string s) {
+
+    }
+
+    char romanNumbers[7]{ 'I','V','X','L','C','D','M' };
+
+#else
+
+    int romanToInt(std::string s) {
+        char space{' '};
+        int value{};
+
+        for (auto i = s.begin() ; i != s.end() ; i++)
+        {
+            s[i] = space;
+            s[i+1] = space;
+        }
+
+        
+
+        return value;
+    }
+
+    char romanNumbers[7]{ 'I','V','X','L','C','D','M' };
+    std::string romanLeftPair[6]{ "IV","IX","XL","XC","CD","CM"};
+
+
+#endif
+
+
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Solution s;
+    qDebug() << (s.romanToInt("III"));
+
+    return a.exec();
+}
